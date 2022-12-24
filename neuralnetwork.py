b701e9189bd463c51ba6081938c02ca98b88b92f
opp1poika <119451400+opp1poika@users.noreply.github.com>
2022-12-24 17:19:15 +0200

    Update neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,25 +1,48 @@
-import numpy as np
-def sigmoid(x):
-  return 1 / (1 + np.exp(-x))
+import random
 
+def s(x):
+    if(x > 0):
+        return x
+    else:
+        return 0
 
-training_inputs = np.array([[0,0,1],
-                            [1,1,1],
-                            [1,0,1],
-                            [0,1,1]])
+def establish_weights(network):
+    weights = []
+    for i in range(len(network)-1):
+        w2 = []
+        for j in range(network[i]):
+            w3 = []
+            if i != len(network) - 2:
+                for k in range(network[i + 1] - 1):
+                    w3.append(random.uniform(-2,2))
+            else:
+                for k in range(network[i + 1]):
+                    w3.append(random.uniform(-2,2))
+            w2.append(w3)
+        weights.append(w2)
+    return weights
 
-training_outputs = np.array([[0,1,1,0]]).T
+def next_layer(input_layer, weights, network, layer_index):
+    output = []
+    for i in range(network[layer_index + 1] - 1):
+        sum = 0
+        for j in range(network[layer_index]):
+            sum += input_layer[j] * weights[layer_index][j][i]
+        output.append(s(sum))
+    if layer_index != len(network) - 2:
+        output.append(1)
+    else:
+        sum = 0
+        for j in range(network[layer_index]):
+            sum += input_layer[j] * weights[layer_index][j][network[layer_index + 1] - 1]
+        output.append(s(sum))
+    return output
 
-np.random.seed(1)
-
-synaptic_weights = 2 * np.random.random((3,1)) - 1
-
-print('Starting weights: ')
-print(synaptic_weights)
-
-for _ in range(1):
-  input_layer = training_inputs
-  
-  outputs = sigmoid(np.dot(input_layer, synaptic_weights))
-  
-print(outputs)
+network = [3, 6, 6, 6, 6, 1]
+weights = establish_weights(network)
+input_layer = [1, 1, 1]
+layer_index = 0
+print(input_layer)
+for i in range(len(network) - 1):
+    input_layer = next_layer(input_layer, weights, network, i)
+    print(input_layer)
